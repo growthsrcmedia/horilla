53836810e09b581a879e234f030d28680385c1a8
Prasanna <77713617+DinoQuinten@users.noreply.github.com>
2026-01-23 13:59:28 +0530

    reduced teh no of workers

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 python3 manage.py createhorillauser --first_name admin --last_name admin --username admin --password admin --email admin@example.com --phone 1234567890
-gunicorn --workers 9 --threads 2 --bind 0.0.0.0:8000 horilla.wsgi:application
+gunicorn --workers 2 --threads 2 --bind 0.0.0.0:8000 horilla.wsgi:application
